fbc401d54d891bbb9c553b1436a3d758726256fd
Cassie C <cassie@colvindesign.com>
2020-11-02 21:31:21 -0500

    Added patron properties

--- src/com/cassiecolvin/hotel/Patron.java
+++ src/com/cassiecolvin/hotel/Patron.java
@@ -1,4 +1,12 @@
 package com.cassiecolvin.hotel;
 
 public class Patron {
+    private static long patronID;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private boolean isPrimaryCardHolder;
+
+    public Patron(){}
 }
