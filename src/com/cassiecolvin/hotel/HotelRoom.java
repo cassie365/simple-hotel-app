33feb1d9522d70eac5078414b035a6c4ade80d27
Cassie C <cassie@colvindesign.com>
2020-11-03 13:43:07 -0500

    added getters/setters for Hotel Room. Modified services and patrons properties datatype to list to utilizing list adding/removing

--- src/com/cassiecolvin/hotel/HotelRoom.java
+++ src/com/cassiecolvin/hotel/HotelRoom.java
@@ -1,11 +1,50 @@
 package com.cassiecolvin.hotel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotelRoom {
     private final int maxCapacity = 4;
-    private Patron[] patrons;
+    private List<Patron> patrons = new ArrayList<Patron>();
     private double pricePerNight;
     private int floor;
-    private Service[] services;
+    private List<Service> services = new ArrayList<Service>();
 
     public HotelRoom() {}
+
+    public List<Patron> getPatrons() {
+        return patrons;
+    }
+
+    public void setPatrons(List<Patron> patrons) {
+        this.patrons = patrons;
+    }
+
+    public void addPatrons(Patron patron){
+        patrons.add(patron);
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public List<Service> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
 }
